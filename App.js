114747ca67f4ac5a69f356c460bf8f3be654496d
TBUk <jonasargrill@gmail.com>
2023-10-27 17:12:46 -0500

    Fix Advertisement Code
    Bluetooth should start advertising, needs further testing

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, NativeModules, Pressable } from 'react-native';
+const { BluetoothPeripheral } = NativeModules;
+import { BleManager } from 'react-native-ble-plx';
+
+const bleManager = new BleManager();
 
 export default function App() {
+  const [devices, setDevices] = useState([]);
+  const startAdvertising = () => {
+    BluetoothPeripheral.advertiseBLE();
+  };
+
+  const startScanning = () => {
+    bleManager.startDeviceScan(null, { allowDuplicates: false }, (error, device) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+  
+      if (device && device.advertising.serviceData && device.advertising.serviceData[0].uuid === '4948be2b-11bc-4b43-9cc5-836c7b65e16b') {
+        console.log(`Found device: ${device.name}`);
+        setDevices(prevDevices => [...prevDevices, device])
+    }});
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={startAdvertising}>
+        <Text style={styles.button}>Start Advertising</Text>
+      </Pressable>
+      <Pressable onPress={startScanning}>
+        <Text style={styles.button}>Start Scanning</Text>
+      </Pressable>
+      <View style={styles.deviceList}>
+        {devices.map(device => (
+          <Text key={device.id}>{device.name}</Text>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +50,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  deviceList: {
+    marginTop: 20,
   },
 });
--- android/app/src/main/java/com/tbuk/MeshChat/BluetoothPeripheral.java
+++ android/app/src/main/java/com/tbuk/MeshChat/BluetoothPeripheral.java
@@ -21,13 +21,16 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.Map;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class BluetoothPeripheral extends ReactContextBaseJavaModule {
     private static final String LOG_TAG = "BluetoothPeripheral";
+    private static final int PERMISSION_REQUEST_CODE = 1;
     BluetoothPeripheral(ReactApplicationContext context) {
         super(context);
     }
@@ -39,7 +42,7 @@ public class BluetoothPeripheral extends ReactContextBaseJavaModule {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @ReactMethod
-    public void advertiseBLE() {
+    public void advertiseBLE() throws InterruptedException {
         Context context = getReactApplicationContext();
         // Start legacy advertising. Works for devices with 5.x controllers,
         // and devices that support multi-advertising.
@@ -49,6 +52,7 @@ public class BluetoothPeripheral extends ReactContextBaseJavaModule {
         AdvertisingSetParameters parameters = (new AdvertisingSetParameters.Builder())
                 .setLegacyMode(true) // True by default, but set here as a reminder.
                 .setConnectable(true)
+                .setScannable(true)
                 .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
                 .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
                 .build();
@@ -80,27 +84,23 @@ public class BluetoothPeripheral extends ReactContextBaseJavaModule {
         };
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(getCurrentActivity(), new String[]{Manifest.permission.BLUETOOTH_ADVERTISE}, PERMISSION_REQUEST_CODE);
+            Log.i(LOG_TAG, "Permissions Denied");
             return;
         }
         advertiser.startAdvertisingSet(parameters, data, null, null, null, callback);
-
+        TimeUnit.SECONDS.sleep(5);
         // After onAdvertisingSetStarted callback is called, you can modify the
         // advertising data and scan response data:
         currentAdvertisingSet.setAdvertisingData(new AdvertiseData.Builder().
                 setIncludeDeviceName(true).setIncludeTxPowerLevel(true).build());
         // Wait for onAdvertisingDataSet callback...
         currentAdvertisingSet.setScanResponseData(new
-                AdvertiseData.Builder().addServiceUuid(new ParcelUuid(UUID.randomUUID())).build());
+                AdvertiseData.Builder().addServiceUuid(new ParcelUuid(UUID.fromString("4948be2b-11bc-4b43-9cc5-836c7b65e16b"))).build());
         // Wait for onScanResponseDataSet callback...
 
         // When done with the advertising:
+        TimeUnit.SECONDS.sleep(60);
         advertiser.stopAdvertisingSet(callback);
     }
 }
